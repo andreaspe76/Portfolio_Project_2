a6a76c7ea4022fa76bb4e4d45acdaecdc28f9174
Andreas Pergantis <andreaspergantis@hotmail.com>
2025-01-25 12:59:18 +0000

    Add event listeners for DOM loading and button click

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -1,3 +1,21 @@
+/**
+ * Wait for the DOM to load before running the game
+ * Get the button elements and add event listeners to them
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons) {
+        button.addEventListener('click', function () {
+            let playerChoice = this.getAttribute('data-choice');
+            alert(`You chose ${playerChoice}`);
+        });
+
+    };
+
+});
+
 function runGame() {
 
 }
