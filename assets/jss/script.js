5a24187605a3551a0d0b8d78a71ab81d3c19059d
Andreas Pergantis <andreaspergantis@hotmail.com>
2025-01-30 10:50:07 +0000

    Add to determineWinner function code to change background color of container element

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -2,7 +2,6 @@
  * Declare constants and variables for DOM elements
  * and possible choices
  */
-
 const buttons = document.getElementsByClassName('btn-control');
 const playerImage = document.getElementById('player-image');
 const computerImage = document.getElementById('computer-image');
@@ -22,7 +21,6 @@ let computerScore = parseInt(document.getElementById('computer-score').innerText
  * Wait for the DOM to load before running the game
  * Get the button elements and add event listeners to them
  */
-
 document.addEventListener('DOMContentLoaded', function () {
 
     for (let button of buttons) {
@@ -38,7 +36,6 @@ document.addEventListener('DOMContentLoaded', function () {
 /**
  * Main game logic
  */
-
 function runGame(playerChoice) {
 
     playerImage.src = `assets/images/${choices[playerChoice]}.webp`;
@@ -57,16 +54,18 @@ function runGame(playerChoice) {
  * Gets the player and computer choices, determines the winner
  * and displays the result
  */
-
 function determineWinner(playerChoice, computerChoice) {
 
     if (playerChoice === computerChoice) {
         result.innerText = 'Its a tie!';
+        document.getElementById('main-container').style.backgroundColor = "rgb(177, 177, 177)";
     } else if (outcomes[playerChoice].includes(computerChoice)) {
         result.innerText = 'Player wins!';
+        document.getElementById('main-container').style.backgroundColor = "rgb(125, 212, 125)";
         incrementPlayerScore();
     } else {
         result.innerText = 'Computer wins!';
+        document.getElementById('main-container').style.backgroundColor = "rgb(238, 104, 115)";
         incrementComputerScore();
     }
 
@@ -75,7 +74,6 @@ function determineWinner(playerChoice, computerChoice) {
 /**
  * Gets the current tally of the user's score from DOM and increments it by 1
  */
-
 function incrementPlayerScore() {
 
     document.getElementById('player-score').innerText = ++playerScore;
@@ -85,7 +83,6 @@ function incrementPlayerScore() {
 /**
  * Gets the current tally of the computer's score from DOM and increments it by 1
  */
-
 function incrementComputerScore() {
 
     document.getElementById('computer-score').innerText = ++computerScore;
